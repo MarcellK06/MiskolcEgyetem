9141b30ec73616adc65dee3c5c7393d305cbb90f
MarcellK06 <150803469+MarcellK06@users.noreply.github.com>
2024-03-20 20:10:20 +0100

    Basic comment

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ for(var k = 1; k < gyongyfilelines.length; k++) {
 
 
 
-
+// SET UP SCENE, CAMERA AND RENDERER
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
